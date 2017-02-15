5de7900a6d23c92793535e7c8eda41351a587ef5
Delacruz21 <mdelacruz1021@gmail.com>
2017-02-15 19:14:06 +0000

    loaded data to dynamodb

---
+++ MoviesLoadData.js
@@ -0,0 +1,27 @@
+var AWS = require("aws-sdk");
+var fs = require('fs');
+
+var dynamodb = new AWS.DynamoDB({ region: 'us-east-1' });
+var client = new AWS.DynamoDB.DocumentClient({ service: dynamodb });
+
+console.log("Importing movies into DynamoDB. Please wait.");
+
+var allMovies = JSON.parse(fs.readFileSync('moviedata.json', 'utf8'));
+allMovies.forEach(function(movie) {
+    var params = {
+        TableName: "Movies",
+        Item: {
+            "year":  movie.year,
+            "title": movie.title,
+            "info":  movie.info
+        }
+    };
+
+    client.put(params, function(err, data) {
+       if (err) {
+           console.error("Unable to add movie", movie.title, ". Error JSON:", JSON.stringify(err, null, 2));
+       } else {
+           console.log("PutItem succeeded:", movie.title);
+       }
+    });
+});
